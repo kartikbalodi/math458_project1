7b4bd967c9969483ebe803d2099d79f4f33172d3
Kartik Balodi <balodi@usc.edu>
2020-10-31 14:10:28 -0700

    Jacobi with print plots

---
+++ q2_Jacobi.m
@@ -0,0 +1,108 @@
+% Assignment:
+% Generates matrix A and b for any N and solves each iteratively
+    % write uij as a vector and then Au=b is a system of equations
+    % We find A and b for any N
+% Jacobi, qn(1) and print plots(Kartik)
+% Gauss-Seidel with over-relaxation parameter (Saket)
+% Conjugate Gradient method (Heather)
+
+%--------------------------------------------------------------------------
+%Start of qn(1) code
+%--------------------------------------------------------------------------
+
+N = 20; % N
+
+% initialize b to g(xij,  yij)
+h = 1/(N+1);
+b = zeros(N^2,1);
+for i = 1:N
+    for j = 1:N
+        if i/(N+1) > 1/5 && i/(N+1) < 3/5 && j/(N+1) > 1/4 && j/(N+1)<1/2
+            b((j-1)*N+i,1) = -h^2; % bij = -h^2*g(xij,yij
+        end
+    end
+end
+
+% filling in A
+A = zeros(N^2,N^2);
+for j = 1:N
+    for i = 1:N
+        % 
+        %    B I 0           4 1 0
+        % A= I B I where B = 1 4 1 , I is I3x3 identity matrix and
+        %    0 I B           0 1 4   0 is 03x3 zero matrix
+        % we use this to get 
+        % 4ui;j - ui+1;j - ui-1;j - ui;j+1 - ui;j-1
+        % fill in identity matrices on the side diagonals
+        c = (j-1)*N;
+        if j > 1
+            A(c+i-N, c+i) = -1;
+        end
+        if j < N
+            A(c+i+N, c+i) = -1;
+        end
+        % then fill in B
+        A(c+i,c+i)= 4;
+        if i ~= 1
+            A(c+i, c+i-1) = -1;
+        end
+        if i ~= N
+            A(c+i, c+i+1) = -1;
+        end
+        
+    end
+end
+
+%--------------------------------------------------------------------------
+% Start of Jacobi Method
+%--------------------------------------------------------------------------
+
+% these are initializations which are used for every program
+atol = 1e-3; % absolute tolerance
+programTimeLimit = 100; % runtime limit in seconds
+x0 = zeros(N^2, 1); % initial guess
+r = norm(b - A *x0, 1); % initial residual (b-A*xk)
+xk = x0; % kth iteration
+iterCount = 0; % number of iterations completed
+
+
+figure(1);
+hold on
+xlabel iterations
+ylabel residual
+
+tic
+% repeat iterations until r <= atol or program time limit reached
+while r > atol 
+    plot(iterCount,r,'kx');
+    time = toc;
+    if time > programTimeLimit
+        break
+    end
+    title(time)
+    drawnow
+    % find the x in next iteration by given formula
+    iterNext = zeros(N^2,1);
+    for i = 1:N^2
+        sum = 0;
+        for j = 1:N^2
+            if j ~= i
+                sum = sum + A(i,j)* xk(j, 1);
+            end
+        end
+        iterNext(i, 1) = (b(i,1)-sum)/A(i,i);
+    end
+    xk = iterNext;
+    r = norm(b - A *xk, 1); % update residual
+    iterCount = iterCount + 1; % increment iteration count
+end 
+% plot one more time for the last pt i.e. first r > atol
+plot(iterCount,r,'kx');
+time = toc;
+title(time)
+drawnow
+
+hold off
+
+
+
