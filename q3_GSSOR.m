718107c74ab9c4cbed09180dc57bc13a898a71df
saketagashe <sagashe@usc.edu>
2020-10-31 22:28:34 -0700

    Update q3_GSSOR.m

--- q3_GSSOR.m
+++ q3_GSSOR.m
@@ -1,38 +1,21 @@
-%------------------------------------------
-%Generating matrix A and b
-%------------------------------------------
-N = 3; % N
+N = 40;
 
-% initialize b to g(xij,  yij)
+% initialize b to the value of g(xij,  yij)
 h = 1/(N+1);
 b = zeros(N^2,1);
 for i = 1:N
     for j = 1:N
         if i/(N+1) > 1/5 && i/(N+1) < 3/5 && j/(N+1) > 1/4 && j/(N+1)<1/2
-            b((j-1)*N+i,1) = -h^2; % bij = -h^2*g(xij,yij
+            b((j-1)*N+i,1) = -h^2;
         end
     end
 end
 
-% filling in A
 A = zeros(N^2,N^2);
 for j = 1:N
     for i = 1:N
-        % 
-        %    B I 0           4 1 0
-        % A= I B I where B = 1 4 1 , I is 3x3 negative identity matrix and
-        %    0 I B           0 1 4   0 is 3x3 zero matrix
-        % we use this to get 
-        % 4ui;j - ui+1;j - ui-1;j - ui;j+1 - ui;j-1
-        % fill in identity matrices on the side diagonals
+        % First take care of the tridiagonal matrices
         c = (j-1)*N;
-        if j > 1
-            A(c+i-N, c+i) = -1;
-        end
-        if j < N
-            A(c+i+N, c+i) = -1;
-        end
-        % then fill in B
         A(c+i,c+i)= 4;
         if i ~= 1
             A(c+i, c+i-1) = -1;
@@ -40,60 +23,72 @@ for j = 1:N
         if i ~= N
             A(c+i, c+i+1) = -1;
         end
-        
+        % Then fill in the identity matrices
+        if j > 1
+            A(c+i-N, c+i) = -1;
+        end
+        if j < N
+            A(c+i+N, c+i) = -1;
+        end
     end
 end
 
+%Set value of omega, can change for tests
+omega = 0.01:0.01:1.99;
+x = zeros(N^2, 1);
 
+least_time = 10000000;
+best_omega = 0;
+time_taken = zeros(length(omega),1);
+for i = i:length(omega)
+    tic;
+    x = SORMethod(A,b,x, omega(i));
+    time = toc;
+    time_taken(i) = time;
+    if time < least_time
+        best_omega = omega(i);
+        least_time = time;
+    end
+end
 
+plot(omega, time_taken)
+xlabel("Values of Omega")
+ylabel("Time Taken to Complete")
 
-
-%Stopping Conditions: a tolerance limit and max number of iterations
-%before quitting
-tolerance = 1e-3;
-max_iterations = 100000;
-omega = 0.01:0.01:2;
-iters = zeros(length(omega), 1);
+function x = SORMethod(A,b,x_init, omega)
+    %Stopping Conditions: a tolerance limit and max number of iterations
+    %before quitting
+    tolerance = 1e-12;
+    max_iterations = 100000;
   
-%LDU Decomoposition of A
-%Isolates lower diagonal (below the main diagonal)
-L = -tril(A, -1);
-%Isolates main diagonal
-D = diag(diag(A));
-%Isolates upper diagonal (above the main diagonal)
-U = -triu(A, 1);
+    %LDU Decomoposition of A
+    %Isolates lower diagonal (below the main diagonal)
+    L = -tril(A, -1);
+    %Isolates main diagonal
+    D = diag(diag(A));
+    %Isolates upper diagonal (above the main diagonal)
+    U = -triu(A, 1);
     
-%Creates initial solution array of zeros
-x0 = zeros(N^2, 1);
-xk = zeros(N^2, 1);
-r = norm(b - A*x0, 1);
+    %Creates initial solution array of zeros
+    x = zeros(length(A));
     
-%Stored in reference since it is used repeatedly
-%A in the equation Ax_i = b where x_i represents the currrent
-%estimation of x
-
+    %Stored in reference since it is used repeatedly
+    %A in the equation Ax_i = b where x_i represents the currrent
+    %estimation of x
+    ref = (D - omega * L);
     
-%Repeats until stopping criterion of iterations is met
-for i = 1:length(omega)
-    num_iterations = 0;
-    while r > tolerance
-        num_iterations = num_iterations + 1;
+    %Repeats until stopping criterion of iterations is met
+    for i = 1:max_iterations
         %Solving the iteration step
-        ref = (D - omega(i) * L);
-        xk = mldivide(ref,((1-omega(i))*D+omega(i)*U)*x0)+omega(i)*mldivide(ref,b);
-        r = norm(b - A*xk, 1); 
-%         ref = (D - omega * L);
-%         xk = mldivide(ref,((1-omega)*D+omega*U)*x0)+omega*mldivide(ref,b);
-%         r = norm(b - A*xk, 1); 
+        x = mldivide(ref,((1-omega)*D+omega*U)*x_init)+omega*mldivide(ref,b);
+        
         %Ends the loop if stopping criterion of tolerance is met
         %Tolerance is the max. acceptable distance between x and x_init
         %(previous estimate of x)
+        if norm(x - x_init) < tolerance
+            break;
+        end
         %x_init stores previous value of x
-        x0 = xk;
+        x_init = x;
     end
-    iters(i) = num_iterations;
 end
-
-plot(omega, iters)
-xlabel("Values of Omega")
-ylabel("Number of Iterations Required")
